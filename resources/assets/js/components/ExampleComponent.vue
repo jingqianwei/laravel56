45893441e48187fc92e3736b586dce4dbe88576f
jqw <jqw@qq.com>
2018-03-13 00:11:23 +0800

    练习在laravel中使用vue.js

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div>
+        <el-button @click="visible = true">Button</el-button>
+        <el-dialog :visible.sync="visible" title="Hello world">
+            <p>Try Element</p>
+        </el-dialog>
+        <text-content></text-content>
+        <cart-content></cart-content>
     </div>
 </template>
 
 <script>
+    Vue.component('text-content', require('./page/TextContent.vue'));
+    Vue.component('cart-content', require('./page/CartContent.vue'));
     export default {
-        mounted() {
-            console.log('Component mounted.')
+        //注意传的值不能为空
+        data() {
+            return { visible: false }
         }
     }
 </script>
