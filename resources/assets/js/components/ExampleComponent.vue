cce1e7e337aaa04691eb8de8f1a0add9d59d3470
jqw <jqw@qq.com>
2018-03-15 09:43:58 +0800

    练习在laravel中使用vue.js

--- resources/assets/js/components/ExampleComponent.vue
+++ resources/assets/js/components/ExampleComponent.vue
@@ -6,12 +6,18 @@
         </el-dialog>
         <text-content></text-content>
         <cart-content></cart-content>
+        <tab-content></tab-content>
+        <checkbox-content></checkbox-content>
+        <cascader-content></cascader-content>
     </div>
 </template>
 
 <script>
     Vue.component('text-content', require('./page/TextContent.vue'));
     Vue.component('cart-content', require('./page/CartContent.vue'));
+    Vue.component('tab-content', require('./page/TabContent.vue'));
+    Vue.component('checkbox-content', require('./page/CheckboxContent.vue'));
+    Vue.component('cascader-content', require('./page/CascaderContent.vue'));
     export default {
         //注意传的值不能为空
         data() {
--- resources/assets/js/components/page/CartContent.vue
+++ resources/assets/js/components/page/CartContent.vue
@@ -12,6 +12,30 @@
                 </div>
             </el-card>
         </el-col>
+        <el-popover
+                ref="popover4"
+                placement="right"
+                width="400"
+                trigger="click">
+            <el-table :data="gridData">
+                <el-table-column width="150" property="date" label="日期"></el-table-column>
+                <el-table-column width="100" property="name" label="姓名"></el-table-column>
+                <el-table-column width="300" property="address" label="地址"></el-table-column>
+            </el-table>
+        </el-popover>
+
+        <el-button v-popover:popover4>click 激活</el-button>
+        <div class="block">
+            <el-pagination
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage"
+                    :page-sizes="[100, 200, 300, 400]"
+                    :page-size="100"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="400">
+            </el-pagination>
+        </div>
     </el-row>
 </template>
 
@@ -21,7 +45,33 @@
             return {
                 title : '好吃的汉堡',
                 button: '操作按钮',
-                currentDate: new Date()
+                currentDate: new Date(),
+                gridData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }],
+                currentPage: 4
+            };
+        },
+        methods: {
+            handleSizeChange(val) {
+                console.log(`每页 ${val} 条`);
+            },
+            handleCurrentChange(val) {
+                console.log(`当前页: ${val}`);
             }
         }
     }
