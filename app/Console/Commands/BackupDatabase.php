294be4a836aa20da683bfec4d4c23adfc941bda1
chinwe.jing <1207511201@qq.com>
2018-08-21 17:05:07 +0800

    定时任务的测试

---
+++ app/Console/Commands/BackupDatabase.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
+class BackupDatabase extends Command
+{
+    protected $process; //进度
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'db:backup';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '备份数据库';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (!file_exists(storage_path('backups'))) {
+            mkdir(storage_path('backups'), 0777);
+        }
+
+        $this->process = new Process(sprintf(
+            'mysqldump -u%s -p%s %s > %s',
+            config('database.connections.mysql.username'),
+            config('database.connections.mysql.password'),
+            config('database.connections.mysql.database'),
+            storage_path('backups') . DIRECTORY_SEPARATOR . 'backup_' . date('Ymd') . '.sql'
+        ));
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        try {
+            $this->process->mustRun();
+            $this->info('The backup has been proceed successfully.');
+        } catch (ProcessFailedException $exception) {
+            dd($exception->getMessage());
+            $this->error('The backup process has been failed.');
+        }
+    }
+}
