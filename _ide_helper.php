75dcfd45bf67e3afee4cff9d8fd7fd39ebde8e7d
chinwe <1207511201@qq.com>
2019-01-27 21:22:04 +0800

    save

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.39 on 2019-01-27 13:12:40.
+ * Generated for Laravel 5.6.39 on 2019-01-27 21:21:19.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
